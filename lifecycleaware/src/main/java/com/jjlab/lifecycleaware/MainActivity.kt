fd27dd59476c0bc6f9df178abfaf04f5bf4c0faa
jps1cob <parakkattilstanly.josejithin@in.bosch.com>
2019-01-01 11:58:06 +0530

    lifecycle aware components, ViewModel, LiveData with Kotlin

--- lifecycleaware/src/main/java/com/jjlab/lifecycleaware/MainActivity.kt
+++ lifecycleaware/src/main/java/com/jjlab/lifecycleaware/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.jjlab.lifecycleaware
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+    }
+
+    private val changeObserver = Observer<Int> {
+        setTimerValue(it)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.restoreState(savedInstanceState)
+
+        // Making viewModel aware of lifecycle of this activity
+        lifecycle.addObserver(viewModel)
+
+        viewModel.changeNotifier.observe(this, changeObserver)
+
+        act_main_btn.setOnClickListener {
+            viewModel.increment()
+        }
     }
+
+    private fun setTimerValue(value: Int) {
+        act_main_timer.text = value.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        viewModel.saveState(outState)
+    }
+
 }
--- lifecycleaware/src/main/java/com/jjlab/lifecycleaware/MainActivityViewModel.kt
+++ lifecycleaware/src/main/java/com/jjlab/lifecycleaware/MainActivityViewModel.kt
@@ -1,7 +1,32 @@
 package com.jjlab.lifecycleaware
 
-import androidx.lifecycle.ViewModel
+import android.os.Bundle
+import androidx.lifecycle.*
 
-class MainActivityViewModel : ViewModel() {
-    
+class MainActivityViewModel(private var count: Int = 0) : ViewModel(), LifecycleObserver {
+
+    companion object {
+        const val COUNT_KEY = "count_key"
+    }
+
+    val changeNotifier = MutableLiveData<Int>()
+
+    fun increment() {
+        changeNotifier.value = ++count
+    }
+
+    fun saveState(outState: Bundle?) {
+        outState?.putInt(COUNT_KEY, count)
+    }
+
+    fun restoreState(inState: Bundle?) {
+        inState?.let { count = it.getInt(COUNT_KEY) }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+        increment()
+    }
+
+
 }
